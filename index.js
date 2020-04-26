53320ea5a71a6bfa09185db9cf7177e24d0ec646
greatshah222 <great.shah222@gmail.com>
2020-04-26 13:10:26 +0300

    read and write file

--- index.js
+++ index.js
@@ -11,6 +11,18 @@ console.log('file has been written');
 
 // Non-blocking async way
 
-fs.readFile('./starter/txt/start.txt', 'utf-8', (err, data) => {
-    console.log(data);
+fs.readFile('./starter/txt/start.txt', 'utf-8', (err, data1) => {
+    if (err) return console.log('error😹🤮');
+    fs.readFile(`./starter/txt/${data1}.txt`, 'utf-8', (err, data2) => {
+        console.log(data2);
+        fs.readFile(`./starter/txt/append.txt`, 'utf-8', (err, data3) => {
+            console.log(data3);
+
+            fs.writeFile('./starter/txt/final.txt', `${data2}\n${data3}`, err => {
+                console.log('file added')
+
+
+            });
+        });
+    });
 });
